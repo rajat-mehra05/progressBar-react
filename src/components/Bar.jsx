d89933670991c43a6c35d02c75b1c4773bd84cb1
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-08-18 14:26:58 +0530

    fix: refactor bar component

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 const Bar = ({ barAmount }) => {
   return (
-    <div
-      className={`border-2 border-black p-4 w-96 rounded-full ${
-        barAmount === 0
-          ? "bg-white"
-          : barAmount < 20
-          ? "bg-red-900"
-          : barAmount < 65
-          ? "bg-yellow-500"
-          : "bg-green-700"
-      }`}
-    ></div>
+    <div className="border-2 border-black rounded-full w-96">
+      <div
+        style={{ width: `${barAmount}%` }}
+        className={`p-4 rounded-full transition ease-in-out delay-10 ${
+          barAmount === 0
+            ? "bg-white"
+            : barAmount < 20
+            ? "bg-red-900"
+            : barAmount < 65
+            ? "bg-yellow-500"
+            : "bg-green-700"
+        }`}
+      ></div>
+    </div>
   );
 };
 
