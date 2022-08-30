b8515ba6540ed267a0260687345f83f7d916f8f2
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-08-30 21:38:48 +0530

    fix: overflow of bar amount

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Bar = ({ barAmount }) => {
   return (
-    <div className="border-2 border-black rounded-full w-96">
+    <div className="border-2 border-black rounded-full w-96 overflow-hidden">
       <div
         style={{ width: `${barAmount}%` }}
         className={`p-4 rounded-full transition ease-in-out delay-10 ${
